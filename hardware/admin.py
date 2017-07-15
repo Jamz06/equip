7efc2fa8a31ea1179367c1f0f62b4e27703d6f89
Lihoded <jamshaha@mail.ru>
2017-07-15 19:52:22 +0500

    Добавлены основные модели. Добавлены в админку.

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Hardware, Types, Hard_Model, Region, Toch_dost
+from .models import Ustr, Types, Hard_Model, Region, Toch_dost, Manufacturer
 
-class RegionInline(admin.StackedInline):
-    model = Region
+class Ustr_Inline(admin.TabularInline):
+    model = Ustr
+    extra = 1
+    
 
 
 class Toch_dostAdmin(admin.ModelAdmin):
@@ -12,7 +14,25 @@ class Toch_dostAdmin(admin.ModelAdmin):
     list_display = ['region', 'name']
     list_filter = ['region', 'name']
     search_fields = ['name']
+    inlines = [Ustr_Inline]
+
+class Hard_Model_Inline(admin.StackedInline):
+    model = Hard_Model
+    extra = 5
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    inlines = [Hard_Model_Inline]
+
+class UstrAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'toch_dost', 'ip',]
+    list_editable = ['toch_dost', 'ip',]
 
 # admin.site.register(Toch_dost, Toch_dostAdmin)
 admin.site.register(Toch_dost, Toch_dostAdmin)
+
 admin.site.register(Region)
+admin.site.register(Ustr, UstrAdmin)
+admin.site.register(Hard_Model)
+admin.site.register(Manufacturer, ManufacturerAdmin)
+admin.site.register(Types)
+
--- hardware/models.py
+++ hardware/models.py
@@ -8,24 +8,44 @@ class Types(models.Model):
     '''
     Тип устройства
     '''
-
+    class Meta:
+        verbose_name = "Тип устройства"
+        verbose_name_plural = "Типы устройств"
+    
     hard_type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.hard_type
+
 
 class Manufacturer(models.Model):
     '''
     Производитель, марка
     '''
+    class Meta:
+        verbose_name = "Марка"
+        verbose_name_plural = "Марки"
+
     name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
 
 
 class Hard_Model(models.Model):
     '''
     Модель устройства
     '''
+    class Meta:
+        verbose_name = "Модель устройства"
+        verbose_name_plural = "Модели устройств"
+
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     hard_type = models.ForeignKey(Types)
 
+    def __str__(self):
+        return self.name
 
 class Region(models.Model):
     '''
@@ -57,19 +77,34 @@ class Toch_dost(models.Model):
     name = models.CharField("Узел связи", max_length=32)
     def __str__(self):
         # return self.region + ", " + self.name
-        return self.name
+        return self.region.name + ", " + self.name
 
-class Hardware(models.Model):
+class Ustr(models.Model):
     '''
     Содержит информацию о железке
     '''
-    toch_dost = models.ForeignKey(Toch_dost)
-    model = models.ForeignKey(Hard_Model)
-    hard_type = models.ForeignKey(Types)
-    ip = models.CharField(max_length=16)
-    mask = models.CharField(max_length=16)
-    login = models.CharField(max_length=20)
-    password = models.CharField(max_length=200)
-    config = models.FileField(storage=storage_configs)
+    class Meta:
+        verbose_name = "Устройство"
+        verbose_name_plural = "Устройства"
+
+    toch_dost = models.ForeignKey(
+        Toch_dost,
+        verbose_name = "Узел свзяи"
+        )
+    model = models.ForeignKey(
+        Hard_Model,
+        verbose_name = "Модель"
+        )
+    hard_type = models.ForeignKey(
+        Types,
+        verbose_name = "Тип"
+        )
+        
+    ip = models.GenericIPAddressField()
+    mask = models.GenericIPAddressField("Маска", default="255.255.255.224")
+    login = models.CharField("Логин", max_length=20, default="admin")
+    password = models.CharField("Пароль", max_length=200)
+    config = models.FileField("Конфиг", storage=storage_configs, blank=True)
     
-    
+    def __str__(self):
+        return self.toch_dost.region.name + ", " + self.toch_dost.name + " " + self.hard_type.hard_type + " " + self.model.name
